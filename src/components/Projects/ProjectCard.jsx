9aaa67feb5b9382f040a75e4f95aa12bdb049956
Anvay Bhanap <anvay.bhanap@gmail.com>
2024-03-24 21:02:26 -0700

    added most projects

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function ProjectCard({ title, description, imgUrl, duration, link }) {
   return (
     <Col size={12} sm={6} md={4}>
-      <a href={link}>
+      <a href={link} target="_blank" rel="noopener noreferrer">
         <div className="proj-imgbx">
           <img src={imgUrl} />
           <div className="proj-txtx">
