9543eb9d1c9c9fe833414e067a575996d0fa4b34
Anvay Bhanap <anvay.bhanap@gmail.com>
2024-04-08 22:12:17 -0700

    removed secondary animations

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -18,10 +18,10 @@ function Contact() {
   const [status, setStatus] = useState({});
 
   const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    })
   }
 
   const handleSubmit = async (e) => {
@@ -38,11 +38,30 @@ function Contact() {
     let result = await response.json();
     setFormDetails(formInitialDetails);
     if (result.code == 200) {
-      setStatus({ succes: true, message: 'Message sent successfully'});
+      setStatus({ succes: true, message: 'Message sent successfully' });
     } else {
-      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+      setStatus({ succes: false, message: 'Something went wrong, please try again later.' });
     }
   };
+
+  // const [hasAnimatedPortfolio, setHasAnimatedPortfolio] = useState(false);
+  //  const [hasAnimatedProfile, setHasAnimatedProfile] = useState(false);
+
+  //   const checkAnimationTriggered = (elementId) => {
+  //     const hasAnimated = sessionStorage.getItem(`hasAnimated_${elementId}`);
+  //     return hasAnimated;
+  //   };
+
+  //   useEffect(() => {
+  //     setHasAnimatedPortfolio(checkAnimationTriggered('portfolio'));
+  //     setHasAnimatedProfile(checkAnimationTriggered('contact'));
+  //   }, []);
+
+  //   useEffect(() => {
+  //     if (hasAnimatedPortfolio) {
+  //       sessionStorage.setItem('hasAnimated_portfolio', 'true');
+  //     }
+  //  }, [hasAnimatedProfile]);
 
   return (
     <section className="contact" id="connect">
@@ -51,42 +70,55 @@ function Contact() {
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                <img
+                  // className={isVisible ? "animate__animated animate__zoomIn" : ""} 
+                  src={contactImg} alt="Contact Us"
+                // onAnimationEnd={() => {
+                //   sessionStorage.setItem('hasAnimated_portfolio', 'true');
+                //   setHasAnimatedPortfolio(true);
+                // }}
+                />
               }
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
-                    </Col>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                    {
-                      status.message &&
-                      <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                <div
+                // className={isVisible ? "animate__animated animate__fadeIn" : ""} 
+                // onAnimationEnd={() => {
+                //   sessionStorage.setItem('hasAnimated_portfolio', 'true');
+                //   setHasAnimatedPortfolio(true);
+                // }}
+                >
+                  <h2>Get In Touch</h2>
+                  <form onSubmit={handleSubmit}>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                       </Col>
-                    }
-                  </Row>
-                </form>
-              </div>}
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                        <button type="submit"><span>{buttonText}</span></button>
+                      </Col>
+                      {
+                        status.message &&
+                        <Col>
+                          <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                        </Col>
+                      }
+                    </Row>
+                  </form>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,9 +15,10 @@ function Projects() {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+              <div 
+              // className={isVisible ? "animate__animated animate__fadeIn": ""}
+              >
                 <h2>Projects</h2>
-                {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
                 <p></p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
@@ -34,7 +35,9 @@ function Projects() {
                       <Nav.Link eventKey="third">Tab 4</Nav.Link>
                     </Nav.Item> */}
                   </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                  <Tab.Content id="slideInUp" 
+                  // className={isVisible ? "animate__animated animate__slideInUp" : ""}
+                  >
                     <Tab.Pane eventKey="first">
                       <Row>
                         {
